b76d7919686c3cb14631a40fa1545254701ccb64
Ali Khatami <khatami7596@gmail.com>
2020-11-11 19:01:41 +0330

    GHappy tests

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,27 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+    GHappy gHappy;
+
+    @BeforeEach
+    public void beforeAllTests(){
+        this.gHappy = new GHappy();
+    }
+
+    @ParameterizedTest(name = "str={0}, result={1}")
+    @CsvSource({"xxggxx,true", "xxgxx,false", "xxggyygxx,false"})
+    public void exampleParameterizedTest(String str, boolean result){
+        Assertions.assertEquals(result, this.gHappy.gHappy(str));
+    }
+
+    @ParameterizedTest(name = "str={0}, result={1}")
+    @CsvSource({"g,false", "x,true", "gg,true", "gx,false", "xg,false"})
+    public void smallInputTest(String str, boolean result){
+        Assertions.assertEquals(result, this.gHappy.gHappy(str));
+    }
 }

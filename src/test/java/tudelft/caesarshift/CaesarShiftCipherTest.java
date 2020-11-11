bb28651116ea7b4df54ecf082c4d6d3d9db41186
Ali Khatami <khatami7596@gmail.com>
2020-11-11 19:59:52 +0330

    unit tests for CaesarShiftCipher

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,33 @@
 package tudelft.caesarshift;
 
-public class CaesarShiftCipherTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+public class CaesarShiftCipherTest {
+    CaesarShiftCipher cipher;
+
+    @BeforeEach
+    public void initialize(){
+        this.cipher = new CaesarShiftCipher();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"abc,3,def", "xyz,3,abc"})
+    public void defaultTests(String message, int shift, String result){
+        Assertions.assertEquals(result, this.cipher.CaesarShiftCipher(message, shift));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"a,-26,a", "z,-27,y"})
+    public void shortInputs(String message, int shift, String result){
+        Assertions.assertEquals(result, this.cipher.CaesarShiftCipher(message, shift));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"aaaa,0,aaaa", "zzzzz,-1,yyyyy"})
+    public void longInputs(String message, int shift, String result){
+        Assertions.assertEquals(result, this.cipher.CaesarShiftCipher(message, shift));
+    }
 }

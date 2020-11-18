b57d16405d691d66b3665c342a97e9bf4c4b6ae3
Ali Khatami <khatami7596@gmail.com>
2020-11-18 22:24:17 +0330

    test TwoNumbersSumTest.java

--- src/test/java/tudelft/sum/TwoNumbersSumTest.java
+++ src/test/java/tudelft/sum/TwoNumbersSumTest.java
@@ -1,5 +1,36 @@
 package tudelft.sum;
 
-public class TwoNumbersSumTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+public class TwoNumbersSumTest {
+    @Test
+    public void testSmallNumbers(){
+        TwoNumbersSum twoNumbersSum = new TwoNumbersSum();
+        ArrayList<Integer> arr1 = new ArrayList<Integer>();
+        ArrayList<Integer> arr2 = new ArrayList<Integer>();
+        arr1.add(1);
+        arr2.add(1);
+        ArrayList<Integer> result = twoNumbersSum.addTwoNumbers(arr1, arr2);
+        int sum = result.stream()
+                .mapToInt(a -> a)
+                .sum();
+        Assertions.assertEquals(2,sum);
+    }
+
+    @Test
+    public void testBigNumbers(){
+        TwoNumbersSum twoNumbersSum = new TwoNumbersSum();
+        ArrayList<Integer> arr1 = new ArrayList<Integer>();
+        ArrayList<Integer> arr2 = new ArrayList<Integer>();
+        arr1.add(100);
+        arr2.add(100);
+        ArrayList<Integer> result = twoNumbersSum.addTwoNumbers(arr1, arr2);
+        int sum = result.stream()
+                .mapToInt(a -> a)
+                .sum();
+        Assertions.assertEquals(200,sum);
+    }
 }
